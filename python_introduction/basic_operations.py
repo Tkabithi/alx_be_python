42c234367f6e6e45794e2d708a55835568e6fde0
Timothy Ngari <Timothykabithi@gmail.com>
2025-05-25 23:46:05 +0300

    basic operations file

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,1 +1,10 @@
+number1 = 10 
+number2 = 5 #variables
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+#printing
+print("Addition of " + str(number1) + " and " + str(number2) + " is " + str(sum_result))
+print("Subtraction of " + str(number1) + " and " + str(number2) + " is " + str(difference_result))
+print("Multiplication of " + str(number1) + " and " + str(number2) + " is " + str(product_result))
 
