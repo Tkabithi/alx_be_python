cc68515f4769356a3f254cb3fc848f1221e29078
Timothy Ngari <Timothykabithi@gmail.com>
2025-05-26 12:05:32 +0300

    simple interest no comments

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,8 +1,6 @@
-#Definition of variables
 principal = 1000
 rate = 0.05
 time = 3
-#Basic calculation
 interest = principal * rate * time
-print("The simple interest is:" + str(interest))
+print("The simple interest is: " + str(interest))
 
