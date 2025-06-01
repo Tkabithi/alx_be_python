579e24d261f7de53c6b960c0697df073f54cb7b1
Timothy Ngari <Timothykabithi@gmail.com>
2025-06-01 23:27:27 +0300

    pattern drawing

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,1 +1,13 @@
+square_size = int(input("Enter the size of the pattern:"))
+i = 0
+
+while i < square_size:
+    for j in range(square_size):
+        print("*",end="")
+    print()
+    i += 1
+
+
+
+
 
